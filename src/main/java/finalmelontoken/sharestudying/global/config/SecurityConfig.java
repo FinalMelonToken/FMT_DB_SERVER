d4c036a8575de7eb5296197cc989a8e5e65871f0
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-08-09 16:23:59 +0900

    clean

--- src/main/java/finalmelontoken/sharestudying/global/config/SecurityConfig.java
+++ src/main/java/finalmelontoken/sharestudying/global/config/SecurityConfig.java
@@ -14,13 +14,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
          http
-                .httpBasic().disable()
-                .csrf().disable()
-                .cors().and()
-                .authorizeRequests()
-                .requestMatchers("/private/**").authenticated() //private로 시작하는 uri는 로그인 필수
-                .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") //admin으로 시작하는 uri는 관릴자 계정만 접근 가능
-                .anyRequest().permitAll(); //나머지 uri는 모든 접근 허용
+                    .httpBasic().disable()
+                    .csrf().disable()
+                    .cors()
+                 .and()
+                    .authorizeRequests()
+                        .requestMatchers("/private/**").authenticated()
+                        .requestMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                    .   anyRequest().permitAll();
          return http.build();
     }
 }
